32d2aaa94060f4cbadc8d67846a1e4031d3f7457
invkrh <invkrh@gmail.com>
2016-12-13 00:55:38 +0100

    Make header as link

--- themes/slim/static/js/data-hacking.js
+++ themes/slim/static/js/data-hacking.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
     $('.post-title a').addClass('hvr-underline-from-left');
     $('.navbar-default .navbar-nav>li>a').addClass('hvr-underline-reveal');
-    $('.header').click(function(){
-        window.location = "/"
-    });
 });
 
 
