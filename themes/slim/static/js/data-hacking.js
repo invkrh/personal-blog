7793549212abb96dbe57a224e9e87cd1bc30781e
invkrh <invkrh@gmail.com>
2016-12-13 01:21:16 +0100

    Use single navbar

--- themes/slim/static/js/data-hacking.js
+++ themes/slim/static/js/data-hacking.js
@@ -1,6 +1,21 @@
+function handleNavbarPosition() {
+    if (window.matchMedia('(max-width: 480px)').matches) {
+        $('#navbar').removeClass('navbar-fixed-top').addClass("navbar-static-top")
+    } else {
+        $('#navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top')
+    }
+}
+
 $(document).ready(function() {
     $('.post-title a').addClass('hvr-underline-from-left');
     $('.navbar-default .navbar-nav>li>a').addClass('hvr-underline-reveal');
+    handleNavbarPosition()
 });
+
+$(window).resize(function() {
+    handleNavbarPosition()  
+});
+
+
 
 
