2aeca9740c96e8acce12303abe382896f48971ed
invkrh <invkrh@gmail.com>
2016-12-23 00:33:46 +0100

    Adjust color, improve event handler performance

--- static/js/main.js
+++ static/js/main.js
@@ -8,23 +8,28 @@ function hideNavBar() {
     $('#navbar').addClass("nav-bar-hidden")
 }
 
+/* 
+ * Show first list term after page loaded
+ */
 // $(window).load(function() {
-//     $('.term .collapse').first().collapse() // Auto show first term
+//     $('.term .collapse').first().collapse() 
 // });
 
-$(window).scroll(function() {
+var scrollTimer = null;
+$(window).scroll(function () {
     if ($(this).scrollTop() == 0) {
         showNavBar()
     } else {
         $("#nav-menu").collapse('hide') // hide collapsible menu when scrolling
         hideNavBar()
-    }
+    }    
 });
 
 $(document).ready(function() {
     $('.post-content > table').addClass('table');
     $('.post-content > table').wrap( "<div class='table-wrapper'></div>" );
-    $('.post-title a').addClass('hvr-underline-from-left');
+    // in case of need to change all html class manually, use js instead
+    $('.post-title a').addClass('hvr-underline-from-left'); 
     $('[data-toggle*="tooltip"]').tooltip()
     $('.avatar').addClass('shake shake-constant');
 
@@ -36,22 +41,18 @@ $(document).ready(function() {
         $(e.currentTarget).find('.label i').removeClass("fa fa-caret-down").addClass("fa fa-caret-left")
     })
 
-    $('#navbar').on('show.bs.collapse', function () {
-       showNavBar()
+    $('#navbar').on('show.bs.collapse', function() { 
+        showNavBar()
+        $(document).one("click", function(event) {
+            var clickover = $(event.target);
+            var _opened = $(".navbar-collapse").hasClass("collapse in");
+            if (_opened === true && clickover.closest('.navbar').attr('id') === undefined) {
+                $('.navbar .collapse').collapse('hide')
+            }
+        });
     })
 
-    $('#navbar').on('hide.bs.collapse', function () {
-        if ($(window).scrollTop() != 0) {
-           hideNavBar()
-        }
+    $('#navbar').on('hide.bs.collapse', function() {
+        if ($(window).scrollTop() != 0) { hideNavBar() }
     })
-
-    // click to dismiss collapsed nav menu
-    $(document).click(function (event) {
-        var clickover = $(event.target);
-        var _opened = $(".navbar-collapse").hasClass("collapse in");
-        if (_opened === true && clickover.closest('.navbar').attr('id') === undefined) {
-            $("button.navbar-toggle").click();
-        }
-    });
 });
